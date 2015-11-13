a707d01a3ce854abeaed55b70a62154322ea88c2
Optimised server <support@eskimosoup.co.uk>
2015-11-13 16:20:11 +0000

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,68 @@
+Rails.application.routes.draw do
+
+  %w( 403 404 422 500 ).each do |code|
+    get code, to: 'errors#show', code: code
+  end
+
+  # This has to be the last route in your list
+  match "*path", to: "errors#show", via: :all, code: 404 unless Rails.application.config.consider_all_requests_local
+
+  mount Optimadmin::Engine => "/admin"
+  root to: 'application#index'
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Example resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Example resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Example resource route with more complex sub-resources:
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', on: :collection
+  #     end
+  #   end
+
+  # Example resource route with concerns:
+  #   concern :toggleable do
+  #     post 'toggle'
+  #   end
+  #   resources :posts, concerns: :toggleable
+  #   resources :photos, concerns: :toggleable
+
+  # Example resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+end
+Optimadmin::Engine.routes.draw do
+end
