fb0ab266a31ffbc231b1fe9f8eff6a90c63976b7
Optimised server <support@eskimosoup.co.uk>
2015-12-11 09:20:07 +0000

    Mobile amends

--- app/assets/javascripts/footer.js
+++ app/assets/javascripts/footer.js
@@ -1,3 +1,5 @@
-$(function() {
-  $('.logo, .primary-navigation-wrap').matchHeight();
-});
+if (Modernizr.mq('only screen and (min-width: 767px)')) {
+  $(function() {
+    $('.logo, .primary-navigation-wrap').matchHeight();
+  });
+}
